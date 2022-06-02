6d737274de34fc3f8fe77124aba332ef71ab08b2
codenm007 <n.majumder14@gmail.com>
2022-06-02 19:22:13 +0530

    added hotel reservation entity

---
+++ src/hotels/reservations.entity.ts
@@ -0,0 +1,36 @@
+
+import { Column, Entity, PrimaryGeneratedColumn ,BeforeInsert } from 'typeorm';
+
+@Entity("reservations")
+export class Hotelreservations {
+  @PrimaryGeneratedColumn({unsigned:true})
+  id: number;
+
+  @Column({type:'datetime'})
+  check_in: Date;
+
+  @Column({type:'datetime'})
+  check_out: Date;
+
+  @Column()
+  no_of_guest: number;
+
+  @Column()
+  no_of_rooms: number;
+
+  @Column()
+  reserved_by: number;
+
+  @Column({type:'datetime'})
+  createdAt: Date;
+
+  @Column({type:'datetime'})
+  updatedAt: Date;
+
+  @Column({ default: false })
+  is_canceled: boolean;
+
+  @Column({ default: false })
+  checkedin: boolean;
+
+}
